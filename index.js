8ca9895695acf8b23e480ef6bb1c9638d52b102a
Vance <coletain@gmail.com>
2021-03-28 22:59:07 -0700

    adds express web server hosting static file with demo frontend

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
 const WebSocket = require('ws');
-const port = 3000;
 
-console.log(`Foundry API Bridge | Listening on: ${port}`);
-const wss = new WebSocket.Server({ port });
+const port = 3000;
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log(`Message Received | ${message}`);
+    wss.broadcast(message);
   });
 
-  ws.send(JSON.stringify({ type: 'handshake' }));
+  // Send handshake confirmation on connect
+  ws.send(JSON.stringify({ topic: 'handshake' }));
 });
+
+wss.broadcast = function broadcast(msg) {
+  console.log(msg);
+  wss.clients.forEach(function each(client) {
+      client.send(msg);
+  });
+};
+
+// Serve up static files dir
+app.use(express.static('pub'));
+
+server.listen(port);
+console.log(`Web server listening on http://localhost:${port}`);
+console.log(`Socket server listening on ws://localhost:${port}`);
